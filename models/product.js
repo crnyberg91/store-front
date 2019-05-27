35feab7739be3b5ac76ea6d287b218c76d460e3b
Chris Nyberg <chrisn83@gmail.com>
2019-05-27 08:47:32 -0400

    product model, boiler plate html w/bootstrap

---
+++ models/product.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+    const Product = sequelize.define("Product", {
+        product: {
+            type: DataTypes.STRING
+        },
+        department: {
+            type: DataTypes.STRING
+        },
+        price: {
+            type: DataTypes.DECIMAL
+        },
+        quantity: {
+            type: DataTypes.INTEGER
+        }
+    });
+
+    return Product;
+}
