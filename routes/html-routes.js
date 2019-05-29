09130f4d8926e190f62fbea741deaf63e9f52904
Chris Nyberg <chrisn83@gmail.com>
2019-05-28 21:43:00 -0400

    html route landing page get

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = (app) => {
 
-    // app.get("/", (req,res)=> {
-    //     res.send()
-    // })
+    app.get("/", (req,res)=> {
+        res.sendFile(path.join(__dirname, "public/index.html"));
+    })
 }
