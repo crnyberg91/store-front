317f74ca8f3ac119c57e1d7178f0f094855ab32b
Chris Nyberg <chrisn83@gmail.com>
2019-05-28 20:57:53 -0400

    schema file with mock data added, changes to html

---
+++ public/js/app.js
@@ -0,0 +1,33 @@
+
+
+function productRender(category, product) {
+  $("#mainContent").empty();
+  $("#mainContent").append(`
+        <div>
+            <ul class="list-group">
+                <li class="list-group-item" id="${category}">${product}</li>
+                <li class="list-group-item" id="${category}">${product}</li>
+                <li class="list-group-item" id="${category}">${product}</li>
+                <li class="list-group-item" id="${category}">${product}</li>
+                <li class="list-group-item" id="${category}">${product}</li>
+            </ul>
+        </div>
+    `);
+};
+
+
+//navbar links
+$("#electronics").on("click", (e) => {
+    e.preventDefault();
+    productRender(electronics, product);
+});
+
+$("#outdoors").on("click", (e) => {
+    e.preventDefault();
+    productRender(outdoors, product);
+});
+
+$("#automotive").on("click", (e) => {
+    e.preventDefault();
+    productRender(autmotive, product);
+});
